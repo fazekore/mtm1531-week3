e360db9ccc80c248e7672ae41294164faddf2484
fazekore <faze0007@algonquinlive.com>
2012-01-23 13:02:15 -0500

    .

--- index.php
+++ index.php
@@ -3,13 +3,25 @@
 $errors = array();
 
 $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
-var_dump($name);
+$email = filter_input(INPUT_POST,'email', FILTER_SANITIZE_EMAIL);
+$message = filter_input(INPUT_POST,'message', FILTER_SANITIZE_STRING);
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	if (empty($name)) {
 		$errors['name'] = true;
 	}
+	
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		$errors['email'] = true;
+	}
+	
+	if (mb_strlen($message) < 25) {
+		$errors['message'] = true;
+	}
+
 }
+
+
 
 ?><!DOCTYPE HTML>
 <html>
@@ -24,11 +36,11 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     <form method="post" action="index.php">
         <div>
             <label for="name">Name<?php if (isset($errors['name'])) : ?> <strong>is required</strong><?php endif; ?></label>
-            <input id="name" name="name" value"<?php echo $name;?>">
+            <input id="name" name="name" value="<?php echo $name;?>" required>
         </div>
         <div>
-            <label for="email">E-mail Address</label>
-            <input type="email" id="email" name="email">
+            <label for="email">E-mail Address<?php if (isset($errors['email'])) : ?> <strong>is required</strong><?php endif; ?></label>
+            <input type="email" id="email" name="email" value="<?php echo $email;?>">
         </div>
         <div>
             <label for="message">Message</label>
