88bca4ccc7d2ae0dc2bb05a7eed3f98743c4d2dc
fazekore <faze0007@algonquinlive.com>
2012-01-23 11:44:51 -0500

    added php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<!DOCTYPE HTML>
+<?php
+
+
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+var_dump($name);
+
+?><!DOCTYPE HTML>
 <html>
 <head>
 <meta charset="utf-8">
@@ -6,10 +12,23 @@
 </head>
 <body>
 
-
-
-
-
+    <form method="post" action="index.php">
+        <div>
+            <label for="name">Name</label>
+            <input id="name" name="name">
+        </div>
+        <div>
+            <label for="email"> E-mail Address</label>
+            <label type="email" id="email" name="email">
+        </div>
+        <div>
+            <label for="message">Message</label>
+            <textarea id="message" name="message"></textarea>
+        </div>
+        <div>
+            <button type="submit">Send Message</button>
+        </div>
+    </form>
 
 </body>
 </html>
